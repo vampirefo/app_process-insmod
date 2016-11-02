980f6548f7dba65a4ccbb600d24a4b07bcfe8f3b
vampirefo <vampirefo@yahoo.com>
2016-11-01 22:43:34 -0400

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := dirtycow
+LOCAL_SRC_FILES := \
+	dirtycow.c
+LOCAL_CFLAGS += -DDEBUG
+LOCAL_SHARED_LIBRARIES := liblog
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := recowvery-app_process32
 LOCAL_SRC_FILES := \
 	recowvery-app_process32.c
