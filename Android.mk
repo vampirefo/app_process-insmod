9149bcdd966ee808f0866a9a2ad2fd9521e1fa05
vampirefo <vampirefo@yahoo.com>
2016-11-01 22:35:25 -0400

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := hijack-app_process64
+LOCAL_MODULE := recowvery-app_process32
 LOCAL_SRC_FILES := \
-	hijack-app_process64.c
+	recowvery-app_process32.c
 LOCAL_CFLAGS += -DDEBUG
 LOCAL_SHARED_LIBRARIES := liblog libcutils libselinux
 LOCAL_LDLIBS := -llog -lselinux -lcutils
